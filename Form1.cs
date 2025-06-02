0b389250c83df444c6ace79c26b2c3a6bf2e014a
Владимир Кудряшов <colder1998@gmail.com>
2025-06-02 12:19:12 +0300

    Добавлен обработчик нажатия button6

--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,29 @@
 using System.Windows.Forms;
 
-namespace RubleAnalysis;
-
-
-
-public partial class Form1 : Form
+namespace RubleAnalysis
 {
-    private zarplata_TABLE salaryData;
-
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-        salaryData = new zarplata_TABLE();
-        button2.Click += button2_Click;
-    }
+        private zarplata_TABLE salaryData;
 
-    private void button2_Click(object sender, EventArgs e)
-    {
-        salaryData.DisplayInDataGridView(dataGridView1);
+        public Form1()
+        {
+            InitializeComponent();
+            salaryData = new zarplata_TABLE();
+            button2.Click += button2_Click;
+            button6.Click += button6_Click;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            salaryData.DisplayInDataGridView(dataGridView1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var (maxGrowth, minGrowth) = salaryData.GetGrowthStats();
+            textBox2.Text = maxGrowth.ToString("0.0") + "%";
+            textBox4.Text = minGrowth.ToString("0.0") + "%";
+        }
     }
 }
