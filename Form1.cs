62b51d7471befe3cb3b54eeb8c7890037c4591dd
Владимир Кудряшов <colder1998@gmail.com>
2025-06-02 12:02:40 +0300

    Добавлен класс с данными зарплаты

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,22 @@
-namespace RubleAnalysis
+using System.Windows.Forms;
+
+namespace RubleAnalysis;
+
+
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private zarplata_TABLE salaryData;
+
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        salaryData = new zarplata_TABLE();
+        button2.Click += button2_Click;
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        salaryData.DisplayInDataGridView(dataGridView1);
     }
 }
---
+++ zarplata_TABLE.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+
+public class zarplata_TABLE
+{
+    private DataTable table;
+
+    public zarplata_TABLE()
+    {
+        // Инициализация таблицы при создании объекта
+        InitializeTable();
+    }
+
+    private void InitializeTable()
+    {
+        table = new DataTable("Медианные зарплаты");
+
+        // Создаем колонки
+        table.Columns.Add("Год", typeof(int));
+        table.Columns.Add("Медианная зарплата (руб.)", typeof(int));
+        table.Columns.Add("Рост (%)", typeof(string));
+
+        // Заполняем данные
+        table.Rows.Add(2010, 15000, "-");
+        table.Rows.Add(2011, 16500, "+10%");
+        table.Rows.Add(2012, 18000, "+9%");
+        table.Rows.Add(2013, 20000, "+11%");
+        table.Rows.Add(2014, 22000, "+10%");
+        table.Rows.Add(2015, 23500, "+6.8%");
+        table.Rows.Add(2016, 25000, "+6.4%");
+        table.Rows.Add(2017, 27000, "+8%");
+        table.Rows.Add(2018, 30000, "+11%");
+        table.Rows.Add(2019, 32500, "+8.3%");
+        table.Rows.Add(2020, 35472, "+9.1%");
+        table.Rows.Add(2021, 40508, "+14.2%");
+        table.Rows.Add(2022, 44724, "+10.4%");
+        table.Rows.Add(2023, 52246, "+16.8%");
+        table.Rows.Add(2024, 58000, "+11%");
+        table.Rows.Add(2025, 63000, "+8.6%");
+    }
+
+    public void DisplayInDataGridView(DataGridView dataGridView)
+    {
+        if (dataGridView != null)
+        {
+            dataGridView.DataSource = table;
+
+            // Настраиваем внешний вид DataGridView
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            // Центрируем столбец с процентами
+            dataGridView.Columns["Рост (%)"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            // Форматируем числовые значения
+            dataGridView.Columns["Медианная зарплата (руб.)"].DefaultCellStyle.Format = "N0";
+        }
+    }
+}
